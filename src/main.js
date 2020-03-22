33e02fe36d558d71a55c76f3967997bf42a18c88
fg-uulm <florian.geiselhart@uni-ulm.de>
2020-03-22 17:36:31 +0100

    Integrated axios

--- src/main.js
+++ src/main.js
@@ -6,12 +6,15 @@ import VueQuagga from "vue-quaggajs";
 import Barcodescanner from "@/components/Barcodescanner.vue";
 import "@/assets/css/tailwind.css";
 import "@/assets/css/main.scss";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
 Vue.config.productionTip = false;
 
 Vue.use(VueQuagga);
 Vue.component("Barcodescanner", Barcodescanner);
 Vue.use(Barcodescanner);
+Vue.use(VueAxios, axios);
 
 new Vue({
   router,
