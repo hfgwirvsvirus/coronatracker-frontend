77d01e77b2b8f9d609afc6b604e933cd6378a221
Radialarray <25952866+Radialarray@users.noreply.github.com>
2020-03-22 19:25:57 +0100

    Timer to remove user above 60 minute time limit implemented

--- src/main.js
+++ src/main.js
@@ -13,8 +13,29 @@ Vue.use(VueQuagga);
 Vue.component("Barcodescanner", Barcodescanner);
 Vue.use(Barcodescanner);
 
-new Vue({
+let vm = new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  mounted: function() {
+    this.$nextTick(function() {
+      // Code that will run only after the
+      // entire view has been rendered
+      setInterval(checkUserlist, 10000);
+    });
+  }
 }).$mount("#app");
+
+function checkUserlist() {
+  let userlist = vm.$store.getters.getUserlist;
+  userlist.forEach(user => {
+    let pastTime = Date.now() - user.timestamp;
+    let minutes = (pastTime / (1000 * 60)).toFixed(0);
+    if (minutes > 60) {
+      this.$store.dispatch("removeUser", {
+        id: user.id,
+        timestamp: Date.now()
+      });
+    }
+  });
+}
